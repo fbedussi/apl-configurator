2e6f98afcf0e207a2f36196d17ebcbc14e7b3764
Francesco Bedussi <francesco@francescobedussi.it>
2017-03-06 22:26:48 +0100

    fix breadcrumbs

--- src/reducers.js
+++ src/reducers.js
@@ -42,18 +42,21 @@ export default function reducer(state = {
 	switch (action.type) {
 		case 'PARSE_ANSWER':
 			let newNode = state.currentNode[action.value];
-
+			let newBreadcrumbs = state.breadcrumbs.concat(state.currentNode);
+			//console.log(newBreadcrumbs);
 			return Object.assign({}, state, {
-				breadcrumbs: state.breadcrumbs.concat(state.currentNode),
+				breadcrumbs: newBreadcrumbs,
 				currentNode: newNode,
 				stepsLeft: depthOf(newNode)
 			});
 		
+		
 		case 'GO_BACK':
 			let backNode = state.breadcrumbs.length >= 1? state.breadcrumbs[state.breadcrumbs.length - 1] : state.currentNode;
-
+			
+			//console.log(state.breadcrumbs.length >= 1? state.breadcrumbs.slice(0, state.breadcrumbs.length - 1) : []);
 			return Object.assign({}, state, {
-				breadcrumbs: state.breadcrumbs.length >= 2? state.breadcrumbs.slice(0, state.breadcrumbs.length - 2) : [],
+				breadcrumbs: state.breadcrumbs.length >= 1? state.breadcrumbs.slice(0, state.breadcrumbs.length - 1) : [],
 				currentNode: backNode,
 				stepsLeft: depthOf(backNode)
 			});
