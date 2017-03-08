fea35120406a7d9c82b9204268aa5ff9d6c357c3
Francesco Bedussi <francesco@francescobedussi.it>
2017-03-08 17:43:13 +0100

    recap progress & new texts.json

--- src/actions.js
+++ src/actions.js
@@ -1,19 +1,22 @@
+import texts from '../texts.json';
+
 export function init(language = 'it') {
-    return function(dispatch) {
-        fetch('texts.json', {
-            mode: 'no-cors'
-        })
-        .then(response => {
-            console.log(response);
-            return response.json();
-        })
-        .then(texts => {
-            dispatch({ type: 'SET_TEXTS', texts })
-        })
-        .catch(function (err) {
-            console.log('ERROR: ', err);
-        });
-    }
+    return { type: 'SET_TEXTS', texts };
+    // return function(dispatch) {
+    //     fetch('texts.json', {
+    //         mode: 'no-cors'
+    //     })
+    //     .then(response => {
+    //         console.log(response);
+    //         return response.json();
+    //     })
+    //     .then(texts => {
+    //         dispatch({ type: 'SET_TEXTS', texts })
+    //     })
+    //     .catch(function (err) {
+    //         console.log('ERROR: ', err);
+    //     });
+    // }
 }
 
 export function parseAnswer(value) {
--- src/components/Recap.js
+++ src/components/Recap.js
@@ -1,23 +1,37 @@
 import React from 'react';
 
+function getAnswerLabel(node, answers, i) {
+    var selectedAnswerKey = answers[i];
+    var selectedAnswerArr = node.answers.filter(answer => {
+        var answerKey = Object.keys(answer)[0];
+        return answerKey === selectedAnswerKey
+    });
+    var selecterAnswerLAbel = selectedAnswerArr[0][selectedAnswerKey];
+    return selecterAnswerLAbel
+}
+
 const Recap = ({ show, breadcrumbs, answers }) => {
     if (!show) {
         return null;
     }
-    
+
     return <table className="recap">
-          {breadcrumbs.map((breadcrumb, i) => <tr>
-            <td>
-                {breadcrumb.text}
-            </td>
-            <td>
-                {breadcrumb.answers.filter(answer => {
-                    var selectedAnswerKey = answers[i];
-                    var currentAnswerKey = Object.keys(answer)[0]; 
-                    return currentAnswerKey === selectedAnswerKey
-                })[0].a}
-            </td>
-              </tr>)}  
+        <thead>
+            <th>
+                <td>Domanda</td>
+                <td>Risposta</td>
+            </th>
+        </thead>
+        <tbody>
+            {breadcrumbs.map((node, i) => <tr>
+                <td>
+                    {node.text}
+                </td>
+                <td>
+                    {getAnswerLabel(node, answers, i)}
+                </td>
+            </tr>)}
+        </tbody>
     </table>
 }
 
