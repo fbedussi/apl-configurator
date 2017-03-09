8954b5a696749b057d46cdb3676b90b10522023c
Francesco Bedussi <francesco@francescobedussi.it>
2017-03-09 16:31:51 +0100

    postcss custom properties

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 
 module.exports = {
   plugins: [
-    require('autoprefixer')({ /* ...options */ })
+    require('autoprefixer')({ /* ...options */ }),
+    require('postcss-custom-properties')
   ]
 }
